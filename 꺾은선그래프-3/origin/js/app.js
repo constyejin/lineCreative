7651d112dab8a49c82cbcc908fe1a520b700a13c
constyejin <lightup.jin@gmail.com>
2024-04-14 23:25:54 +0900

    ✨  화면 로딩 됐을 때 실행할 함수

--- 꺾은선그래프-3/origin/js/app.js
+++ 꺾은선그래프-3/origin/js/app.js
@@ -1,6 +1,31 @@
-import {setCommonRoot, autoScale, toggleFullScreen, createDraggable, getScale} from './common.js';
-import anime from "./anime.js";
+import {setCommonRoot, autoScale, toggleFullScreen} from './common.js';
+// import anime from "./anime.js";
 
 var metaUrl = import.meta.url;
 var root = null;
+
+
+window.addEventListener('script-loaded', function(ev) {	
+  if (root) return;
+  const u = new URL(metaUrl);
+  const param = u.searchParams.get("embed-unique");
+
+  if (param && param !== ev.detail.unique) return;
+
+  const shadowRoot = ev.detail.root; // 커스텀 이벤트에 담겨진 shadowRoot 객체
+  root = shadowRoot;
+
+  setCommonRoot(root, {});
+	autoScale();
+	
+	window.addEventListener("resize", function () {
+		autoScale();
+	});
+
+
+	root.querySelector("#fullScreenButton").addEventListener("click", () => {
+    toggleFullScreen();
+  });
+});
+
 
