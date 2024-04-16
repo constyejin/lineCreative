bb2f404794a333c4759727f90c95a13fb7420759
constyejin <lightup.jin@gmail.com>
2024-04-17 01:51:42 +0900

    ⚡️  rowItemLeng 최소,최대값에 맞춰서 버튼 활성, 비활성화

--- graph3/origin/js/app.js
+++ graph3/origin/js/app.js
@@ -26,33 +26,11 @@ window.addEventListener('script-loaded', function(ev) {
     toggleFullScreen();
   });
 
-
   // select-unit active
   let selectUnitTxt = root.querySelectorAll('.select-unit-txt');
   let selectUnit = root.querySelector('.select-unit');
   let selectUnitItem = selectUnit.querySelectorAll('li');
   let rowItem = root.querySelectorAll('.left-box-2 .row');
-  
-  rowItem.forEach(function(item) {
-    let rowItemLeng = item.querySelectorAll('li').length;
-
-    let defaultLeftValue = 582;
-    let additionalLeftValue = -78 * (rowItemLeng - 1);
-    let leftValue = defaultLeftValue + additionalLeftValue;
-    selectUnit.style.left = leftValue + 'px';
-  })
-
-  selectUnitTxt.forEach(function(item, num) {
-    item.addEventListener('click', function() {
-      if(num == 0) {
-        selectUnit.style.top = '186px';
-      } else if(num == 1) {
-        selectUnit.style.top = '246px';
-      }
-      selectUnitTxt = item;
-      selectActive(selectUnitTxt);
-    })
-  })
  
   function selectActive(selectItem) {
     root.querySelector('#viewWrap').addEventListener('click', function(e) {
@@ -72,6 +50,73 @@ window.addEventListener('script-loaded', function(ev) {
       })
     })
   }
+
+  selectUnitTxt.forEach(function(item, num) {
+    item.addEventListener('click', function() {
+      if(num == 0) {
+        selectUnit.style.top = '186px';
+      } else if(num == 1) {
+        selectUnit.style.top = '246px';
+      }
+      selectUnitTxt = item;
+      selectActive(selectUnitTxt);
+    })
+  })
+
+  // 그래프 칸 수에 따라 팝업 포지션 변경 (기본 3개)
+  rowItem.forEach(function(item) {
+    let rowItemLeng = item.querySelectorAll('li').length;
+
+    let defaultLeftValue = 582;
+    let additionalLeftValue = -78 * (rowItemLeng - 1);
+    let leftValue = defaultLeftValue + additionalLeftValue;
+    selectUnit.style.left = leftValue + 'px';
+
+    let btnMinus = root.querySelector('.btn-minus');
+    let btnPlus = root.querySelector('.btn-plus');
+  
+    btnMinus.addEventListener('click', function() {
+      if (rowItemLeng > 2) {        
+        item.querySelector('li:last-child').remove();
+        rowItemLeng = item.querySelectorAll('li').length;
+        additionalLeftValue = -78 * (rowItemLeng - 1);
+        leftValue = defaultLeftValue + additionalLeftValue;
+        selectUnit.style.left = leftValue + 'px';
+      }
+
+      if(rowItemLeng < 8) {
+        btnPlus.querySelector('img').src = new URL('../img/Status=On-1.png', metaUrl).href;
+      }
+
+      if(rowItemLeng <= 2) {
+        btnMinus.querySelector('img').src = new URL('../img/Status=Off.png', metaUrl).href;
+      }
+    })
+
+    btnPlus.addEventListener('click', function() {
+      if (rowItemLeng < 8) {
+        // 새로운 li 요소 생성
+        let newLi = document.createElement('li');
+        // 새로운 li에 내용 추가
+        newLi.textContent = '';
+        // li를 마지막으로 추가
+        item.appendChild(newLi);
+        // 추가 후 다시 left 값을 업데이트
+        rowItemLeng = item.querySelectorAll('li').length;
+        additionalLeftValue = -78 * (rowItemLeng - 1);
+        leftValue = defaultLeftValue + additionalLeftValue;
+        selectUnit.style.left = leftValue + 'px';
+      }
+
+      if(rowItemLeng > 2) {
+        btnMinus.querySelector('img').src = new URL('../img/Status=On.png', metaUrl).href;
+      }
+
+      if (rowItemLeng === 8) {
+        btnPlus.querySelector('img').src = new URL('../img/Status=Off-1.png', metaUrl).href;
+      }
+    })
+  })
 
 
   // 물결선 checked
