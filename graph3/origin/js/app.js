a0304b6ed81baada64ad4318009c9dc66dac1d47
constyejin <lightup.jin@gmail.com>
2024-04-19 01:05:30 +0900

    🚧 plusItem, minusItem 함수 분리 및 코드 정리중

--- graph3/origin/js/app.js
+++ graph3/origin/js/app.js
@@ -30,7 +30,6 @@ window.addEventListener('script-loaded', function(ev) {
   let selectUnitTxt = root.querySelectorAll('.select-unit-txt');
   let selectUnit = root.querySelector('.select-unit');
   let selectUnitItem = selectUnit.querySelectorAll('li');
-  let rowItem = root.querySelectorAll('.left-box-2 .row');
  
   function selectActive(selectItem) {
     root.querySelector('#viewWrap').addEventListener('click', function(e) {
@@ -77,7 +76,13 @@ window.addEventListener('script-loaded', function(ev) {
     })
   })
 
-  rowItem.forEach(function(item) {
+  let rowItem = root.querySelectorAll('.left-box-2 .row');
+  let btnMinus = root.querySelector('.btn-minus');
+  let btnPlus = root.querySelector('.btn-plus');
+  let plusItem; 
+  let minusItem;
+
+  rowItem.forEach((item) => {
     let rowItemLeng = item.querySelectorAll('li').length;
 
     let defaultLeftValue = 582;
@@ -85,41 +90,11 @@ window.addEventListener('script-loaded', function(ev) {
     let leftValue = defaultLeftValue + additionalLeftValue;
     selectUnit.style.left = leftValue + 'px';
 
-    let btnMinus = root.querySelector('.btn-minus');
-    let btnPlus = root.querySelector('.btn-plus');
-    let maxNotice = root.querySelector('.max-notice');
     
+    let maxNotice = root.querySelector('.max-notice');
     let clickCount = 3;
 
-    btnMinus.addEventListener('click', function() {
-      if (clickCount >= 9) {
-        clickCount -= 2;
-      } else  if(clickCount > 1) {
-        clickCount--;
-      }
-
-      if (rowItemLeng > 2) {        
-        item.querySelector('li:last-child').remove();
-        rowItemLeng = item.querySelectorAll('li').length;
-        additionalLeftValue = -78 * (rowItemLeng - 1);
-        leftValue = defaultLeftValue + additionalLeftValue;
-        selectUnit.style.left = leftValue + 'px';
-      }
-
-      if(rowItemLeng < 9) {
-        btnPlus.querySelector('img').src = new URL('../img/Status=On-1.png', metaUrl).href;
-        maxNotice.style.display = 'none';
-        selectUnit.classList.remove('last');
-      }
-
-      if(rowItemLeng <= 2) {
-        btnMinus.querySelector('img').src = new URL('../img/Status=Off.png', metaUrl).href;
-      }
-    })
-
-
-    // 칸 플러스, 마이너스
-    btnPlus.addEventListener('click', function() {
+    plusItem = function () {
       if(clickCount < 9) {
         clickCount++;
       }
@@ -158,7 +133,133 @@ window.addEventListener('script-loaded', function(ev) {
           maxNotice.style.display = 'none';
         },3000)
       }
-    })
+    }
+
+    minusItem = function () {
+      if (clickCount >= 9) {
+        clickCount -= 2;
+      } else  if(clickCount > 1) {
+        clickCount--;
+      }
+
+      if (rowItemLeng > 2) {        
+        item.querySelector('li:last-child').remove();
+        rowItemLeng = item.querySelectorAll('li').length;
+        additionalLeftValue = -78 * (rowItemLeng - 1);
+        leftValue = defaultLeftValue + additionalLeftValue;
+        selectUnit.style.left = leftValue + 'px';
+      }
+
+      if(rowItemLeng < 9) {
+        btnPlus.querySelector('img').src = new URL('../img/Status=On-1.png', metaUrl).href;
+        maxNotice.style.display = 'none';
+        selectUnit.classList.remove('last');
+      }
+
+      if(rowItemLeng <= 2) {
+        btnMinus.querySelector('img').src = new URL('../img/Status=Off.png', metaUrl).href;
+      }
+    }
+  })
+
+
+  btnPlus.addEventListener('click', () => {
+    plusItem();
+  })
+
+  btnMinus.addEventListener('click', () => {
+    minusItem();
+  })
+
+
+
+
+
+
+  rowItem.forEach(function(item) {
+    // let rowItemLeng = item.querySelectorAll('li').length;
+
+    // let defaultLeftValue = 582;
+    // let additionalLeftValue = -78 * (rowItemLeng - 1);
+    // let leftValue = defaultLeftValue + additionalLeftValue;
+    // selectUnit.style.left = leftValue + 'px';
+
+    // let btnMinus = root.querySelector('.btn-minus');
+    // let btnPlus = root.querySelector('.btn-plus');
+    // let maxNotice = root.querySelector('.max-notice');
+    
+    // let clickCount = 3;
+
+    // btnMinus.addEventListener('click', function() {
+    //   if (clickCount >= 9) {
+    //     clickCount -= 2;
+    //   } else  if(clickCount > 1) {
+    //     clickCount--;
+    //   }
+
+    //   if (rowItemLeng > 2) {        
+    //     item.querySelector('li:last-child').remove();
+    //     rowItemLeng = item.querySelectorAll('li').length;
+    //     additionalLeftValue = -78 * (rowItemLeng - 1);
+    //     leftValue = defaultLeftValue + additionalLeftValue;
+    //     selectUnit.style.left = leftValue + 'px';
+    //   }
+
+    //   if(rowItemLeng < 9) {
+    //     btnPlus.querySelector('img').src = new URL('../img/Status=On-1.png', metaUrl).href;
+    //     maxNotice.style.display = 'none';
+    //     selectUnit.classList.remove('last');
+    //   }
+
+    //   if(rowItemLeng <= 2) {
+    //     btnMinus.querySelector('img').src = new URL('../img/Status=Off.png', metaUrl).href;
+    //   }
+    // })
+
+
+    // // 칸 플러스, 마이너스
+    // btnPlus.addEventListener('click', function() {
+    //   if(clickCount < 9) {
+    //     clickCount++;
+    //   }
+
+    //   if (rowItemLeng < 9) {
+    //     let newItem = `
+    //       <li>
+    //         <input type="text">
+    //       </li>
+    //     `;
+
+    //     item.insertAdjacentHTML('beforeend', newItem);
+        
+    //     rowItemLeng = item.querySelectorAll('li').length;
+    //     additionalLeftValue = -78 * (rowItemLeng - 1);
+    //     leftValue = defaultLeftValue + additionalLeftValue;
+    //     selectUnit.style.left = leftValue + 'px'
+    //   }
+
+    //   if(rowItemLeng > 2) {
+    //     btnMinus.querySelector('img').src = new URL('../img/Status=On.png', metaUrl).href;
+    //   }
+
+    //   if (rowItemLeng === 9) {
+    //     btnPlus.querySelector('img').src = new URL('../img/Status=Off-1.png', metaUrl).href;
+
+    //     if (selectUnit) {
+    //       selectUnit.classList.add('last');
+    //       selectUnit.style.left = '110px';
+    //     }
+    //   }
+
+    //   if(clickCount >= 9) {
+    //     maxNotice.style.display = 'block';
+    //     setTimeout(function() {
+    //       maxNotice.style.display = 'none';
+    //     },3000)
+    //   }
+    // })
+
+
   })
 
 
@@ -243,8 +344,9 @@ window.addEventListener('script-loaded', function(ev) {
 
 
 
-  // Reset
+  // 전체 초기화
   let resetBtn = root.querySelector('.reset-btn');
+
 	resetBtn.addEventListener('click', () => {
     // 타이틀 초기화
     hideTitle();
