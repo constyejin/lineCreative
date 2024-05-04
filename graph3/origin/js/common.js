54c64ab7d476609fdc08f23a28403d285fcd23fc
constyejin <lightup.jin@gmail.com>
2024-05-04 11:55:06 +0900

    ✨ 전체화면 ESC로 종료시 아이콘 변경

--- graph3/origin/js/common.js
+++ graph3/origin/js/common.js
@@ -11,43 +11,49 @@ export function setCommonRoot(shadowRoot, functions) {
 
 /* default end */
 export function autoScale(){
-    const findWidth = root.querySelector('#viewWrap');
-    const width = findWidth.offsetWidth; // 너비
-    const height = findWidth.offsetHeight; // 높이
-    const givenWidth = 1920; // 주어진 너비
-    const givenHeight = 1020; // 주어진 높이
-    const targetWidth = width; // 원하는 너비
-    const targetHeight = height; // 원하는 높이
-    // 주어진 너비에서의 스케일 계산
-    const calculatedScaleWidth = (targetWidth / givenWidth) * 100;
-    const calculatedScaleHeight = (targetHeight / givenHeight) * 100;
-    // width, height 중에서 더 작은 값을 기준으로 스케일 적용
-    const calculatedScale = Math.min(calculatedScaleWidth, calculatedScaleHeight);
-    const wrap = root.querySelector(".wrap");
-    wrap.style.transform = "scale(" + calculatedScale + "%)";
-    scale = calculatedScale;
+  const findWidth = root.querySelector('#viewWrap');
+  const width = findWidth.offsetWidth; // 너비
+  const height = findWidth.offsetHeight; // 높이
+  const givenWidth = 1920; // 주어진 너비
+  const givenHeight = 1020; // 주어진 높이
+  const targetWidth = width; // 원하는 너비
+  const targetHeight = height; // 원하는 높이
+  // 주어진 너비에서의 스케일 계산
+  const calculatedScaleWidth = (targetWidth / givenWidth) * 100;
+  const calculatedScaleHeight = (targetHeight / givenHeight) * 100;
+  // width, height 중에서 더 작은 값을 기준으로 스케일 적용
+  const calculatedScale = Math.min(calculatedScaleWidth, calculatedScaleHeight);
+  const wrap = root.querySelector(".wrap");
+  wrap.style.transform = "scale(" + calculatedScale + "%)";
+  scale = calculatedScale;
 }
 
 export function getScale() {
-    return scale;
+  return scale;
 }
 
 export function toggleFullScreen() {
-    var doc = document;
-    var docEl = root.firstElementChild;
-  
-    var requestFullScreen = doc.documentElement.requestFullscreen || doc.mozRequestFullScreen || doc.webkitRequestFullScreen || doc.msRequestFullscreen;
-    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
-  
+  var doc = document;
+  var docEl = root.firstElementChild;
+
+  var requestFullScreen = doc.documentElement.requestFullscreen || doc.mozRequestFullScreen || doc.webkitRequestFullScreen || doc.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+    
+    // 전체화면 이미지로 변경
+    root.querySelector('#fullScreenButton > img').src = new URL('../img/btn-fullscreen-off.png', metaUrl).href;
+  } else {
+    cancelFullScreen.call(doc);
+    // 일반 이미지로 변경
+    root.querySelector('#fullScreenButton > img').src = new URL('../img/btn-fullscreen-on.png', metaUrl);
+  }
+
+  doc.addEventListener('fullscreenchange', function() {
     if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
-      requestFullScreen.call(docEl);
-      
-      // 전체화면 이미지로 변경
-      root.querySelector('#fullScreenButton > img').src = new URL('../img/btn-fullscreen-off.png', metaUrl).href;
-    } else {
-      cancelFullScreen.call(doc);
-      // 일반 이미지로 변경
       root.querySelector('#fullScreenButton > img').src = new URL('../img/btn-fullscreen-on.png', metaUrl);
     }
+  });
 }
 
